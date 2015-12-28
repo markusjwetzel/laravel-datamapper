e0ee8a71afc916a0d2a2ad0235ca6243fc80b62f
Koga1988 <markuswetzel@gmx.net>
2015-12-28 16:50:21 +0100

    Improve magical getters

--- src/Support/Model.php
+++ src/Support/Model.php
@@ -25,9 +25,26 @@ abstract class Model implements ModelContract
      */
     public function __call($method, $parameters)
     {
-        // magical getter
-        if (isset($this->{$method}) || property_exists($this, $method)) {
+        // magical getter for objects
+        if ((isset($this->{$method}) || property_exists($this, $method))&&
+            (is_object($this->{$method}) || is_array($this->{$method})))
+        {
             return $this->{$method};
+        }
+
+        // magical getter for scalars
+        $scalarProperty = (substr($method, 0, 3) == 'get')
+            ? lcfirst(substr($method, 3))
+            : null;
+        
+        if ($scalarProperty && (isset($this->{$scalarProperty}) || property_exists($this, $scalarProperty)))
+        {
+            if (is_scalar($this->{$scalarProperty})) {
+                return $this->{$scalarProperty};
+            }
+            if ($this->{$scalarProperty} instanceof \ProAI\Datamapper\Support\ValueObject) {
+                return (string) $this->{$scalarProperty};
+            }
         }
 
         $class = get_class($this);
